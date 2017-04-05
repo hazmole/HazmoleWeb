a405b230dae29a086e85cb409db54dd643eb55fe
zhutong <buzzfirefox@gmail.com>
2017-04-06 05:23:29 +0800

    FUCK

--- react_js/DeviceList.js
+++ react_js/DeviceList.js
@@ -1,6 +1,75 @@
 
 // 2017.04.06 - Hazmole modified
 //===================================
+// Content
+//===================================
+class MainContent extends React.Component {
+  /* Constructor */
+  constructor(props) {
+    // Set Props
+    super(props);
+  }
+
+  /* UI Display */
+  render() {
+      return (
+        <div className="MainContent">
+          <div className="list-group">
+            Page: {this.props.name}
+          </div>
+        </div>
+      );
+  }
+}
+//===================================
+// DeviceNav
+//===================================
+class DeviceNav extends React.Component {
+  /* Constructor */
+  constructor(props) {
+    // Set Props
+    super(props);
+    // Set State
+    this.state = { 
+      "selected": "default"
+    };
+
+    // Connect functions
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  /* Funcitons */
+  handleSelect(selected_info) {
+    this.setState({"selected": selected_info});
+  }
+
+  componentWillReceiveProps(nextProps, nextState){
+    this.setState({"selected": "default"});
+  }
+  /* UI Display */
+  render() {
+      var nav_bar = null;
+      if(this.props["data-sel_type"]=="group")
+        nav_bar = ["Topology", "Data Usage", "Signal Strength", "Channel Interference", "DFS", "WiFi Scan"];
+      else if(this.props["data-sel_type"]!=null)
+        nav_bar = ["Data Usage", "Signal Strength", "Diagnose"];
+      else
+        nav_bar = [];
+
+      const listItems = nav_bar.map((group) =>
+        <li role="presentation"><a href="#" onClick={this.handleSelect.bind(null,group)}>{group}</a></li>
+      );
+      return (
+        <div>
+          <ul className="nav nav-tabs">
+            {listItems}
+          </ul>
+          <MainContent name={this.state.selected}/>
+        </div>
+      );
+  }
+}
+//===================================
 // DeviceList
 //===================================
 class DeviceList extends React.Component {
@@ -82,7 +151,7 @@ class DeviceList extends React.Component {
             <div id="Navigator">{listItems}</div>
           </div>
           <div className="col-md-10">
-            
+            <DeviceNav data-sel_type={this.state.selected_type} data-sel_id={this.state.selected_id} />
           </div>
         </div>
       );
